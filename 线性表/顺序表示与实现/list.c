988d8dfae0122d0bef2dcfe0f9c0724ef1231075
谢庭宇 <qyyzxty@icloud.com>
2020-10-20 22:00:14 +0800

    add list

--- 线性表/顺序表示与实现/list.c
+++ 线性表/顺序表示与实现/list.c
@@ -32,14 +32,31 @@ int ListInsert(list *L, int e) {
         L->elem=newbase;
         L->size+=LISTINCREASEMENT; 
     }
+
     int i;
     for (i = 0; i < L->length; i++)
     {
-        if (e>L->elem[i])
-        {
-            break;  
-        }
-        
+        if (e<L->elem[i])
+            break;
+    }
+
+    int ex;
+    int j=0;
+    for (j = i; j < L->length; j++) {
+        ex=L->elem[j];
+        L->elem[j]=e;
+        e=ex;
+    }
+
+    L->elem[j] = e;
+    L->length++;
+    return i; 
+}
+
+void ListPrint(list * L) {
+    for (int i = 0; i < L->length; i++)
+    {
+        printf("%d\n", L->elem[i]);
     }
     
 }
@@ -47,6 +64,15 @@ int ListInsert(list *L, int e) {
 int main() {
     list *list_sq;
     InitList(list_sq);
+
+    ListInsert(list_sq, 1);
+    ListInsert(list_sq, 5);
+    ListInsert(list_sq, 4);
+    ListInsert(list_sq, 2);
+    ListInsert(list_sq, 3);
+    ListInsert(list_sq, 6);
+    
+    ListPrint(list_sq);
     return 0;
 }
 
